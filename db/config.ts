56300bcf2b6cb2bc905ceed1253d5333bd93800e
Pablo Robalino <pabloth@live.com>
2024-09-03 17:02:16 -0500

    cambio en config y seed DB

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,22 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from 'astro:db';
 
-// https://astro.build/db/config
+const Gato = defineTable({
+  columns: {
+    id: column.text({primaryKey: true}),
+    name: column.text(),
+    age: column.number(),
+    dueno_cedula: column.text({references: () => Dueno.columns.cedula})
+  },
+})
+
+const Dueno = defineTable({
+  columns: {
+    cedula: column.text({primaryKey: true}),
+    name: column.text(),
+    age: column.number(),
+  },
+})
+
 export default defineDb({
-  tables: {}
+  tables: {Gato, Dueno}
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,38 @@
-import { db } from 'astro:db';
+import { db, Dueno, Gato } from 'astro:db';
 
-// https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+	await db.insert(Dueno).values([
+		{
+			name: 'Juan',
+			age: 25,
+			cedula: '1234567890'
+		},
+		{
+			name: 'Pedro',
+			age: 30,
+			cedula: '0987654321',
+		}
+	])
+
+	await db.insert(Gato).values([
+		{
+			id: "dfghjkl",
+			name: 'Michi',
+			age: 2,
+			dueno_cedula: '1234567890'
+		},
+		{
+			id: "asdfghj",
+			name: 'Mecha',
+			age: 3,
+			dueno_cedula: '0987654321'
+		},
+		{
+			id: "qwertyu",
+			name: 'Miche',
+			age: 4,
+			dueno_cedula: '1234567890'
+		}
+	])
+
 }
